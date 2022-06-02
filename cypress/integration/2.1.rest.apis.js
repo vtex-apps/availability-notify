65a460d23bd19c75edec0715eac4a607bb922bc1
Shashidhar Reddy <shashidhargujjula@gmail.com>
2022-06-02 16:48:14 +0530

    rest apis testcases

--- cypress/integration/2.1.rest.apis.js
+++ cypress/integration/2.1.rest.apis.js
@@ -1,12 +1,18 @@
 import {
+  notifySearch,
   processAllRequest,
   processUnsentRequest,
+  updateProductStatus,
 } from '../support/availability-notify.apis'
 import { testSetup } from '../support/common/support'
+import { testCase1 } from '../support/availability-notify.outputvalidation'
+const { data1, data2 } = testCase1
 
 describe('Rest api', () => {
   testSetup()
 
-  processUnsentRequest()
-  processAllRequest()
+  // processUnsentRequest()
+  // processAllRequest()
+  updateProductStatus(data1, true)
+  notifySearch()
 })
--- cypress/support/availability-notify.apis.js
+++ cypress/support/availability-notify.apis.js
@@ -1,5 +1,6 @@
+import { updateProductStatusAPI } from './product.api'
+import { VTEX_AUTH_HEADER, FAIL_ON_STATUS_CODE } from './common/constants'
 import { updateRetry } from './common/support'
-import { FAIL_ON_STATUS_CODE } from './common/constants'
 
 export function processUnsentRequest() {
   it('verify the unsend request', updateRetry(3), () => {
@@ -25,6 +26,40 @@ export function processAllRequest() {
     }).then((response) => {
       cy.log(response)
       expect(response.status).to.equal(200)
+    })
+  })
+}
+
+export function updateProductStatus(data1, unlimited = false) {
+  it('update the product status', updateRetry(3), () => {
+    cy.addDelayBetweenRetries(2000)
+    cy.getVtexItems().then((vtex) => {
+      cy.request({
+        method: 'PUT',
+        url: updateProductStatusAPI(data1),
+        headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+        ...FAIL_ON_STATUS_CODE,
+        body: { unlimitedQuantity: unlimited, quantity: 0 },
+      }).then((response) => {
+        expect(response.body).to.be.true
+      })
+    })
+  })
+}
+
+export function notifySearch() {
+  it('Notify search', updateRetry(3), () => {
+    cy.addDelayBetweenRetries(2000)
+
+    cy.getVtexItems().then((vtex) => {
+      cy.request({
+        method: 'GET',
+        url: 'https://sandboxusdev.myvtex.com/api/dataentities/notify/search?_schema=reviewsSchema&_fields=email,skuId,name,createdAt',
+        headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+        ...FAIL_ON_STATUS_CODE,
+      }).then((response) => {
+        expect(response.status).to.equal(200)
+      })
     })
   })
 }
