6303c42ec378c77a1e163d2d3c89150c637918f5
saravanan <saravananvenkatesan@bitcot.com>
2022-06-02 18:08:36 +0530

    added unavailable_product

--- cypress/support/availability-notify.selectors.js
+++ cypress/support/availability-notify.selectors.js
@@ -4,4 +4,6 @@ export default {
     '.vtex-store-components-3-x-subscriberContainer input[name=email]',
   AvailabilityNotifySubmitButton: '.vtex-availability-notify-1-x-submit button',
   AvailabilityNotifyAlert: '.vtex-availability-notify-1-x-success',
+  name: '.vtex-availability-notify-1-x-inputName > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input',
+  email: '.vtex-availability-notify-1-x-inputEmail > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input'
 }
