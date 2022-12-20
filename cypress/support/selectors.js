244b0a3e0f502ad0c8d74af980e326204332d552
Shashidhar Reddy <shashidhargujjula@gmail.com>
2022-12-20 18:17:48 +0530

    fixes

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,6 +1,6 @@
 export default {
   AvailabilityNotifySubmitButton: 'div[class*="submit"] button',
   AvailabilityNotifyAlert: 'div[class*=success]',
-  name: 'div[class*="subscriberContainer"] input[name="name"]',
-  email: 'div[class*="subscriberContainer"] input[name="email"]',
+  name: 'div[class*="notiferContainer"] input[name="name"]',
+  email: 'div[class*="notiferContainer"] input[name="email"]',
 }
